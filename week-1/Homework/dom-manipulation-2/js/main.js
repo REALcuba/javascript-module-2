16133f81860d6ab3631574899c203bc0a1aaabd7
REALcuba <angelskcuba@gmail.com>
2022-04-23 00:57:42 +0200

    js2 H dom2 blue button all colors done

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,19 +1,23 @@
 //create function to change blue Button background
 
 const createFuntionButtonBlue = () => {
-const jumbotron = document.querySelector(".jumbotron")
- const blueBtn = document.getElementById("blueBtn")
-blueBtn.addEventListener("click", () => changeBackgrounColor() )
+  const jumbotron = document.querySelector(".jumbotron")
+  const blueBtn = document.getElementById("blueBtn")
+  const btnPrimary = document.querySelector(".btn-lrg")
+
+  const btnSecondary = document.querySelector(".btn-secondary")
+  blueBtn.addEventListener("click", () => changeColorsBlueBtn())
 
 
-function changeBackgrounColor() {
-     jumbotron.style.backgroundColor= "red"
-//    blueBtn.style.backgroundColor = `#583fbd`;
-//     console.log("potato")
-//     return jumbotron
-}
+  function changeColorsBlueBtn() {
+    jumbotron.style.backgroundColor = "#588fbd";
+    btnPrimary.style.backgroundColor = "#ffa500";
+    btnSecondary.style.backgroundColor = "black";
+    btnSecondary.style.color = "white";
 
-return blueBtn
+  }
+
+  return blueBtn
 
 }
 
