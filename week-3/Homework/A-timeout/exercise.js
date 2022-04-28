72dbc133027119e7d494abc67ef538d7ab96c8a8
REALcuba <angelskcuba@gmail.com>
2022-04-28 10:54:18 +0200

    working with timeout

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,17 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+function setBackgroundColor() {
+    setTimeout(setColor(), 5000);
+
+    function setColor() {
+        const bodyColor = document.getElementById("main")
+        bodyColor.style.backgroundColor = "red"
+    }
+
+}
+function myStopFunction() {
+    clearTimeout(setBackgroundColor);
+}
+setBackgroundColor()
