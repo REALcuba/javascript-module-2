2d89a07e9171ea299dcd0bdf4f9219094a630be2
REALcuba <angelskcuba@gmail.com>
2022-05-25 14:51:29 +0200

    test

--- week-2/Homework/Part-2/G-object-loops/exercise-1.js
+++ week-2/Homework/Part-2/G-object-loops/exercise-1.js
@@ -3,11 +3,24 @@
 // Try to use both methods shown above to achieve this
 
 const studentGrades = {
-    tom: 20,
-    george: 17,
-    abdul: 19,
-  };
-  
+  tom: 20,
+  george: 17,
+  abdul: 19,
+};
+function showGradeMoreThan18() {
+  let arr = studentGrades.forEach(student => {
+    if (student.grade > 18) {
+      console.log(grade);
+    }
+
+    return arr
+
+  });
+  console.log(arr);
+}
+showGradeMoreThan18()
+// console.log(studentGrades.abdul);
+
   // Prints
   // TOM - 20
   // ABDUL - 19
